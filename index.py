d7787b7ca24d97b08a3957283fac0474f2ae3098
janeluck <janeluck158@gmail.com>
2016-08-12 10:07:29 +0800

    init

---
+++ index.py
@@ -0,0 +1,13 @@
+a = 100
+if a > 0:
+    print('python')
+b = input() or 20
+if int(b) < 20 :
+    print('young')
+else:
+    print('modest')
+
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
