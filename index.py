4f0a52affd1247a45f7355b7f9d6274859b9bad5
janeluck <janeluck158@gmail.com>
2016-08-13 22:00:49 +0800

    learning grammer

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 a = 100
 if a > 0:
     print('python')
-b = input() or 20
-if int(b) < 20 :
+#b = input() or 20
+if int(10) < 20 :
     print('young')
 else:
     print('modest')
@@ -11,3 +11,21 @@ sum = 0
 for x in range(101):
     sum = sum + x
 print(sum)
+
+# tuple
+c = (1, 2, 3)
+print(c)
+
+# list
+d = [1, 2, 3]
+print(d)
+
+# dict
+e = {'jane': 25, 'jack': 29, 'rose': 31 }
+
+# set 要创建一个set，需要提供一个list作为输入集合(自动过滤掉重复的key)
+f = set([1, 2, 3, 4, 5, 1, 5])
+print(f)
+
+# g = set([2, 3, 4, 5, d]) unhashable type: 'list'
+
