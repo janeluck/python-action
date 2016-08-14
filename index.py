1a03551d53c57947ffeabbd29d94b8d995ba098c
janeluck <janeluck158@gmail.com>
2016-08-14 15:29:55 +0800

    generator

--- index.py
+++ index.py
@@ -165,3 +165,21 @@ def fib(max):
 
 for n in fib(6):
     print(n)
+
+print('-----------------------')
+# Python的for循环本质上就是通过不断调用next()函数实现的
+for x in [1, 2, 3, 4, 5]:
+    pass
+
+
+# 首先获得Iterator对象:
+it = iter([1, 2, 3, 4, 5])
+# 循环:
+while True:
+    try:
+        # 获得下一个值:
+        x = next(it)
+        print(x)
+    except StopIteration:
+        # 遇到StopIteration就退出循环
+        break
