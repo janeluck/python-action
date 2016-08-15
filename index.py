ec551918fb555846e3605495b77deef641d02e8a
janeluck <janeluck158@gmail.com>
2016-08-15 13:18:01 +0800

    inheritance and class Dog(Animal):

--- index.py
+++ index.py
@@ -216,6 +216,19 @@ class Lesson(object):
 
 print(Lesson('Math').get_name())
 
+# Polymorphism
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+
+
+class Dog(Animal):
+
+    def run(self):
+        print('Dog is running...')
+
+    def eat(self):
+        print('Eating meat...')
 
 
 
