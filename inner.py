f24fe9860bed576b55cda12d98572a8a89eb91a4
janeluck <janeluck158@gmail.com>
2016-08-21 10:20:49 +0800

    zip

--- inner.py
+++ inner.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from collections import deque
 import hashlib
+import itertools
+
 print(datetime(2016, 10, 1, 8, 30))
 print(datetime.now())
 q = deque(['a', 'b', 'c'])
@@ -16,7 +18,13 @@ print(md5.hexdigest())
 md5.update('in python hashlib?'.encode('utf-8'))
 print(md5.hexdigest())
 
+naturals = itertools.count(2)
+ns = itertools.takewhile(lambda x: x <= 10, naturals)
+for n in ns:
+    print(n)
 
+for c in itertools.chain('abc', 'def'):
+    print(c)
 
-
-
+for z in zip([1, 2, 3], [9, 8, 7]):
+    print(z)
