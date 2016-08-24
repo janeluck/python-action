3e7bb16f9c82390811dfb5fec707d238636ca778
janeluck <janeluck158@gmail.com>
2016-08-24 21:37:34 +0800

    variableArguments

--- variableArguments.py
+++ variableArguments.py
@@ -7,3 +7,12 @@ def cal(*numbers):
 
 nums = [1, 2, 3, 4, 5]
 print(cal(*nums))
+
+
+# 在Python中定义函数，可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这5种参数都可以组合使用。但是请注意，参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数
+def f1(a, b, c=0, *args, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw)
+
+
+def f2(a, b, c=0, *, d, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'd =', d, 'kw =', kw)
